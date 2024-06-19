d6f04da786de1ae704fb3c4d232049730babe12b
Metche216 <drmatiasetcheverry@gmail.com>
2024-06-19 00:51:57 -0400

    no puedo hacer que se descargue el json de las noticias

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from datetime import datetime, timedelta
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+#stock api key
 APIKEY = "ZUUPNI5KQ09BVIS1"
+#news api key
+newsAPI = "aadf084dcc73405e80648b9175777c1f"
 
 parameters = {
     "apikey": APIKEY,
@@ -11,21 +14,36 @@ parameters = {
     "symbol": STOCK,
 }
 
-url = 'https://www.alphavantage.co/query'
-r = requests.get(url, params=parameters)
+news_parameters = {
+    "q": (COMPANY_NAME),
+    "from": 2024-6-19,
+    "apiKey": newsAPI,
+
+}
+
+stock_url = 'https://www.alphavantage.co/query'
+news_url = ('https://newsapi.org/v2/everything')
+       
+r = requests.get(stock_url, params=parameters)
 time_series = r.json()["Time Series (Daily)"]
-
+ts_sorted = sorted(time_series)
     
-today = datetime.today().date()
-yesterday = (today - timedelta(1)).strftime('%Y-%m-%d')
-day_before_yesterday = (today - timedelta(2)).strftime('%Y-%m-%d')
+# today = datetime.today().date()
+# yesterday = (today - timedelta(1)).strftime('%Y-%m-%d')
+# day_before_yesterday = (today - timedelta(2)).strftime('%Y-%m-%d')
 
-today_price = float(time_series[str(today)]["4. close"])
-yesterday_price = float(time_series[yesterday]["4. close"])
 
-if today_price > yesterday_price*1.05 or today_price < yesterday_price*0.95:
-    print("value has changed drastically...")
-print(today_price, yesterday_price)
+# today_price = float(ts_sorted[0]["4. close"])
+# yesterday_price = float(ts_sorted[1]["4. close"])
+
+#if today_price > yesterday_price*1.05 or today_price < yesterday_price*0.95:
+    
+response = requests.get(news_url, params=news_parameters)
+data = response.json()
+print(data)
+
+
+
 # if yesterday in time_series and day_before_yesterday in time_series:
 #     yesterday_price = time_series[yesterday]["close"]
 #     day_before_yesterday = time_series[day_before_yesterday]["close"]
@@ -35,8 +53,6 @@ print(today_price, yesterday_price)
 
 
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
